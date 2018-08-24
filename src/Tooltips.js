c13aeddec839de328cfa3f7ff7d24e07fd71e210
unknown <Administrator@DESKTOP-5MDRRK1.allsworth.com>
2018-08-24 10:42:39 +0800

    zmk-2

--- src/Tooltips.js
+++ src/Tooltips.js
@@ -3,19 +3,25 @@ import './Tooltips.css';
 
 
 class Tooltips extends Component {
+
   render() {
-    
-    console.log(this.props.commit.author);
+    const { commit } = this.props; 
+
     return (
+      
       <div className='tooltip'>
         <div className="commit">
-            <div className="avatar"></div>
+            <img className="avatar" 
+              src={"https://www.gravatar.com/avatar/" + commit.author.email}
+              alt={commit.author.name}
+              title={commit.author.name} 
+            />
             <div className="content">
-                <h3 className="message"> </h3>
+                <h3 className="message"> {commit.message} </h3>
                 <p>
-                    <span className="data"></span>
+                    <span className="data">{commit.date}</span>
                     -
-                    <span className='sha'></span>
+                    <span className='sha'>{commit.sha}</span>
                 </p>
             </div>
         </div>

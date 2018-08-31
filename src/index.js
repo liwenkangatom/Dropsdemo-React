577b72e2f3930b3ea2993d60b483e0a615da0e5d
willadam <liwenkang4757@outlook.com>
2018-08-31 18:17:51 +0800

    merge 2

--- src/index.js
+++ src/index.js
@@ -1,15 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-<<<<<<< HEAD
 import registerServiceWorker from './registerServiceWorker';
 import App from './App';
 import './style.js';
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
-=======
-import './index.css';
-import registerServiceWorker from './registerServiceWorker';
-import App from './App';
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
->>>>>>> b4e615940b280eaf3b280c9f4c028c9cd82674a0
